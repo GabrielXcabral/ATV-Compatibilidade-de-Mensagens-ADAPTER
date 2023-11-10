ba2c3a973614b353f9522ca08d5f91cd317a3448
GabrielXcabral <gabriel.cabral@academico.ifpb.edu.br>
2023-11-10 15:25:03 -0300

    Main feito

--- src/AppSMSNotification.java
+++ src/AppSMSNotification.java
@@ -1,5 +1,4 @@
-import api.SMSSender;
-import api.TimService;
+import api.*;
 import model.SMS;
 
 public class AppSMSNotification {
@@ -7,9 +6,15 @@ public class AppSMSNotification {
 	public static void main(String[] args) {
 		SMSSender sender = new TimService();
 		
+		VivoSender vivoSender = new VivoService();
+		SMSSender vivoadapter = new VIVOAdapter(vivoSender);
+		
 		SMS message = new SMS("83988885544","83988221133","Bom dia. Seu boleto ja esta disponivel para pagamento");
 		
-		sender.sendSMS(message);		
+		sender.sendSMS(message);
+		System.out.println("\n");
+		System.out.println("VIVO ADAPTER: " + "\n");
+		vivoadapter.sendSMS(message);
 		
 		
 	}
